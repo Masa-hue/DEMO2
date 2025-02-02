c73ba004801906dfeeb256694b1a45ec3944b96d
Masa <135356339+Masa-hue@users.noreply.github.com>
2025-02-02 16:03:40 +0100

    Add files via upload

--- productsPage.js
+++ productsPage.js
@@ -8,6 +8,13 @@ class ProductsPage {
         this.cartBadge = By.className('shopping_cart_badge');
         this.cartIcon = By.className('shopping_cart_link');
         this.checkoutButton = By.id('checkout');
+        this.firstNameField = By.id('first-name');
+        this.lastNameField = By.id('last-name');
+        this.zipCodeField = By.id('postal-code');
+        this.continueButton = By.id('continue');
+        this.finishButton = By.id('finish');
+        this.burgerButton = By.id('react-burger-menu-btn');
+        this.logoutButton = By.id('logout_sidebar_link');
     }
 
     async addToCart(productIndex) {
@@ -22,11 +29,35 @@ class ProductsPage {
 
     async goToCart() {
         await this.driver.findElement(this.cartIcon).click();
+
     }
 
     async checkout() {
         await this.driver.findElement(this.checkoutButton).click();
     }
+
+    async fillCheckoutForm(firstName, lastName, zipCode) {
+        await this.driver.findElement(this.firstNameField).sendKeys(firstName);
+        await this.driver.findElement(this.lastNameField).sendKeys(lastName);
+        await this.driver.findElement(this.zipCodeField).sendKeys(zipCode);
+    }
+
+    async submitCheckout() {
+        await this.driver.findElement(this.continueButton).click();
+    }
+
+    async finishCheckout() {
+        await this.driver.findElement(this.finishButton).click();
+        
+    }
+
+    async finishburgerButton() {
+        await this.driver.findElement(this.burgerButton).click();
+    }
+    async finishlogoutBotton() {
+        await this.driver.findElement(this.logoutButton).click();
+    }
+    
 }
 
 module.exports = ProductsPage;
